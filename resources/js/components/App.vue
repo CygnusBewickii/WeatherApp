6774df7c7f9a94dc195ef7f4d3ba7389dbbdc71e
Artem <68503862+CygnusBewickii@users.noreply.github.com>
2022-07-07 01:42:57 +0300

    init commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <Header></Header>
+    </div>
+</template>
+
+<script>
+import Header from "./layouts/Header.vue";
+export default {
+    name: "App",
+    components: {
+       Header,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
